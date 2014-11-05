a85b68133c181571308b893d7e0b7eeb5fced622
Constantine Kiriaze <ckiriaze@gmail.com>
2014-11-05 15:58:39 -0800

    merge

--- lib/functions/plugins.php
+++ lib/functions/plugins.php
@@ -2,4 +2,3 @@
 
 // Recommended Plugins and installation
 require_once locate_template( '/lib/functions/tgm-plugin-activation/class-tgm-plugin-activation.php', true );
-require_once locate_template( '/lib/functions/tgm-plugin-activation/required_plugins.php', true );
