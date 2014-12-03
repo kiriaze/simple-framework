4c3107760a4eef599c33b52c096f2cd1617f096d
Constantine Kiriaze <ckiriaze@gmail.com>
2014-12-02 22:58:43 -0800

    updated setup, removed theme support and added to child theme, works cleaner this way.

--- lib/functions/setup.php
+++ lib/functions/setup.php
@@ -17,7 +17,7 @@
  */
 
 
-add_action( 'after_setup_theme', 'simple_setup', 9 );
+add_action( 'after_setup_theme', 'simple_setup', 10 );
 function simple_setup() {
 
 	// Text Domain / Localization
@@ -45,26 +45,6 @@ function simple_setup() {
 		'gallery',
 		'image'
 	));
-
-	// Post Thumbnails (Featured Images / Sizes)
-	add_theme_support( 'post-thumbnails' );
-
-	// Simple Framework Supports
-	add_theme_support('simple-relative-urls');  	//  Enable relative URLs
-	add_theme_support('simple-rewrites');       	// 	Enable URL rewrites for only parent theme
-	add_theme_support('simple-breadcrumbs'); 		//  Enable breadcrumbs
-	add_theme_support('debug'); 					//  Enable debug bar
-	add_theme_support('admin_bar'); 				//  Enable admin bar
-	add_theme_support('jquery-cdn');            	//  Enable to load jQuery from the Google CDN. Issue with infinite scroll if enabled, include migrate
-
-	// remove_theme_support in child theme if undesired, all enabled by default
-	add_theme_support('custom_searchform');			//	Enable use of custom searchform template - /templates/searchform.php
-	add_theme_support('nice-search');				//	Enables clean search in url; from /?s= to /search/result
-	add_theme_support('theme-options-setup');		//	Enable Setup tab in theme options
-	add_theme_support('more-themes-link');			//	Enable more theme links under dashboard menu
-	add_theme_support('admin-footer-text');			//	Enable extra text in admin footer
-	add_theme_support('remove_admin_menu_items');	//	Remove Unwanted Admin Menu Items(left hand side)
-	add_theme_support('remove_admin_bar_links');	//	Remove Unwanted Admin Menu Items(from admin bar)
 
 	//  Redirect to theme options on theme activation
 	if ( is_admin() && isset( $_GET['activated'] ) && 'themes.php' == $GLOBALS['pagenow'] ) {
