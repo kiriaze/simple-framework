954bc54e416a33f869b2352b2c49831765410396
Constantine Kiriaze <ckiriaze@gmail.com>
2014-12-11 20:23:26 -0800

    added dope menu functions

--- lib/functions/menus.php
+++ lib/functions/menus.php
@@ -41,10 +41,11 @@ if( !function_exists('simple_menu_output') ) {
 
 }
 
+// initial menu registration in admin
 add_action('after_setup_theme', 'initial_menu_setup', 10);
 function initial_menu_setup() {
 
-	// register them in dropdown
+	// register menus in dropdown
 	$menus = get_registered_nav_menus();
 	foreach ( $menus as $key => $value ) {
 
@@ -64,4 +65,114 @@ function initial_menu_setup() {
 		}
 	}
 
+}
+
+// Get Nested Menu Array Output
+// Usage
+// $menu = new NestedMenu('menu-name');
+// foreach ( $menu->items as $item ) :
+// sp($item);
+// $submenu = $menu->get_submenu($item);
+// if ( $submenu ) :
+// foreach ( $submenu as $subitem ) :
+// sp($subitem);
+// endforeach;
+// endif;
+// endforeach;
+class NestedMenu {
+
+    private $flat_menu;
+    public $items;
+
+    function __construct($name) {
+
+		$this->flat_menu = wp_get_nav_menu_items($name);
+		$this->items     = array();
+
+		if ( $this->flat_menu ) {
+			foreach ( $this->flat_menu as $item ) {
+			    if ( !$item->menu_item_parent ) {
+			        array_push($this->items, $item);
+			    }
+			}
+		}
+
+    }
+
+    public function get_submenu($item) {
+        $submenu = array();
+        foreach ( $this->flat_menu as $subitem ) {
+            if ( $subitem->menu_item_parent == $item->ID ) {
+                array_push($submenu, $subitem);
+            }
+        }
+        return $submenu;
+    }
+
+}
+
+
+
+// Return sub menus only, accepts multiple through arrays
+// array(
+// 	'menu'      => 'mega-menu',
+// 	'submenu'   => array('Page Name'),
+// 	'container' => '',
+// );
+add_filter('wp_nav_menu_objects', 'nav_submenu_objects_filter', 10, 2);
+function nav_submenu_objects_filter( $items, $args ) {
+	// $loc should be an array of items. if it's empty, move along
+	$loc = isset( $args->submenu ) ? $args->submenu : '';
+	if ( !isset($loc) || empty($loc) ) {
+		return $items;
+	}
+	if ( is_string($loc) ) {
+		$loc = split("/", $loc);
+	}
+	if ( empty($loc) ) {
+		return $items;
+	}
+
+ 	// prepare a slug for every item
+	foreach ( $items as $item ) {
+		if ( empty($item->slug) ) {
+			$item->slug = sanitize_title_with_dashes($item->title);
+		}
+	}
+
+	//  find the selected parent item ID(s)
+	$cursor = 0;
+	foreach ( $loc as $slug ) {
+		$slug = sanitize_title_with_dashes($slug);
+		foreach ( $items as $item ) {
+			if ( $cursor == $item->menu_item_parent && $slug == $item->slug ) {
+				$cursor = $item->ID;
+				continue 2;
+			}
+		}
+		return array();
+	}
+
+ 	//  walk finding items until all levels are exhausted
+	$parents = array($cursor);
+	$out     = array();
+
+	while ( !empty($parents) ) {
+
+		$newparents = array();
+
+		foreach ( $items as $item ) {
+			if ( in_array( $item->menu_item_parent, $parents ) ) {
+				if ( $item->menu_item_parent == $cursor ) {
+					$item->menu_item_parent = 0;
+				}
+				$out[]        = $item;
+				$newparents[] = $item->ID;
+			}
+		}
+
+		$parents = $newparents;
+	}
+
+	return $out;
 }
